568095a9edce10508257f4e63ff4bd2f54255c3b
Graham Martin <graham@pixely.co.uk>
2020-04-19 00:14:23 +0100

    feat: add nav animations

--- src/components/molecules/nav/index.js
+++ src/components/molecules/nav/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
+import React, { useState, useCallback, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import GenericNav from './genericNav';
 
@@ -8,27 +9,70 @@ import './_nav.scss';
 
 const Nav = ({ className, navItems }) => {
   const [menuActive, setMenu] = useState(false);
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const handleEscape = useCallback((event) => {
+    if(event.keyCode === 27) {
+      setMenu(false);
+    }
+  }, []);
+
+  const setMenuStatus = (status) => {
+    setMenuVisible(status);
+    if(!status) {
+      setTimeout(() => setMenu(false), 300);
+    } else {
+      setMenu(status);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleEscape, false);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscape, false);
+    };
+  }, []);
+
+  useEffect(() => {
+    menuVisible && document.body.classList.add('is-locked');
+
+    return () => {
+      document.body.classList.remove('is-locked');
+    };
+  }, [menuVisible]);
+
+  const buttonOffset = () => {
+    let offset = null;
+    if ( menuActive && window.scrollY < 10 ) {
+      offset = { transform: `translateY(${ window.scrollY }px)` };
+    }
+    return offset;
+  }
 
   return (
     <>
       <button
         className={`${className ? `${className} ` : ''}nav${menuActive ? ' is-open' : ''}`}
-        onClick={() => setMenu(!menuActive)}
+        onClick={() => setMenuStatus(!menuActive)}
         aria-label="Open main menu"
+        style={buttonOffset()}
       >
         <span className="nav__icon-bar"></span>
         <span className="nav__icon-bar"></span>
         <span className="nav__icon-bar"></span>
         <span className="nav__icon-bar"></span>
       </button>
-      <nav className={`nav__menu${menuActive ? ' is-open' : ''}`}>
-        <GenericNav navItems={navItems} navClass="nav__menu-item" />
-        <GenericNav navItems={navItems} navClass="nav__menu-item" />
-        <GenericNav navItems={navItems} navClass="nav__menu-item" />
-        <GenericNav navItems={navItems} navClass="nav__menu-item" />
-        <GenericNav navItems={navItems} navClass="nav__menu-item" />
-        <GenericNav navItems={navItems} navClass="nav__menu-item" />
-      </nav>
+      {menuActive && <div key="oa" className={`nav__overlay ${menuVisible ? 'is-open' : ''}`} onClick={() => setMenuStatus(!menuActive)} /> }
+      {menuActive && <nav key="ob" className={`nav__menu ${menuVisible ? 'is-open' : ''}`}>
+          <GenericNav navItems={navItems} navClass="nav__menu-item" />
+          <GenericNav navItems={navItems} navClass="nav__menu-item" />
+          <GenericNav navItems={navItems} navClass="nav__menu-item" />
+          <GenericNav navItems={navItems} navClass="nav__menu-item" />
+          <GenericNav navItems={navItems} navClass="nav__menu-item" />
+          <GenericNav navItems={navItems} navClass="nav__menu-item" />
+        </nav>
+      }
     </>
   )
 }
