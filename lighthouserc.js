3d8920d3961f041c52bc5e3e24c043d273800281
Graham Martin <graham@pixely.co.uk>
2020-06-06 17:40:01 +0100

    chore: fix linting

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,6 @@ module.exports = {
             target: 'lhci',
             serverBaseUrl: 'https://enigmatic-chamber-98135.herokuapp.com',
             token: 'f198d45e-d61f-4281-816c-90c736f88908',
-      },
+        },
     },
 }
