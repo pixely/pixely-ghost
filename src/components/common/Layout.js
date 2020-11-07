db8af31ddace45a8f209b11c18296c6572a0704c
Graham Martin <graham@pixely.co.uk>
2020-11-07 19:46:47 +0000

    chore: switch away from google fonts plugin

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -20,6 +20,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <style type="text/css">{`${site.codeinjection_styles}`}</style>
                 <script>{`${site.codeinjection_head}`}</script>
                 <body className={`base${bodyClass ? ` base--${bodyClass}` : ``} ${bodyClass}`} />
+                <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&family=Karla:ital,wght@0,400;0,700;1,400&family=Rubik:ital,wght@0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
             </Helmet>
             <Base data={data} config={config} isHome={isHome}>
                 {children}
