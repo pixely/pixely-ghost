79893d5890144c94ed7bca6c1427de3a24eb0877
Graham Martin <graham@pixely.co.uk>
2021-01-07 23:41:42 +0000

    chore: mkdir before writing file

--- src/_data/fonts.js
+++ src/_data/fonts.js
@@ -1,5 +1,5 @@
 const Cache = require('@11ty/eleventy-cache-assets')
-const { writeFile } = require('fs')
+const { mkdir, writeFile } = require('fs')
 
 module.exports = async function () {
     const url = 'https://fonts.googleapis.com/css2?family=Anonymous+Pro&family=Karla:ital,wght@0,400;0,700;1,400&family=Rubik:ital,wght@0,700;1,400;1,700&display=swap'
@@ -15,6 +15,13 @@ module.exports = async function () {
         },
     })
 
+    mkdir('./dist/styles', { recursive: true }, (err) => {
+        if (err) {
+            return console.log(err)
+        }
+        return null
+    })
+    
     writeFile(`./dist${filePath}`, fontCss, (err) => {
         if (err) {
             return console.log(err)
