4f3843c808c5189a96939b00cf02978d785ffdfd
Graham Martin <graham@pixely.co.uk>
2021-01-07 21:27:20 +0000

    chore: add decoding=async to images

--- .eleventy.js
+++ .eleventy.js
@@ -79,7 +79,7 @@ const responsiveImageShortcode = async (src, alt, srcset=null, sizes, loading="l
       ${Object.values(resized).map(imageFormat => {
         return `  <source type="image/${imageFormat[0].format}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}" />`;
       }).join('')}
-      <img src="${lowsrc.url}" width="${lowsrc.width}" height="${lowsrc.height}" loading="${loading}" alt="${alt}" class="image ${className}" />
+      <img src="${lowsrc.url}" width="${lowsrc.width}" height="${lowsrc.height}" loading="${loading}" alt="${alt}" class="image ${className}" decoding="async" />
     </picture>`;
 };
 
