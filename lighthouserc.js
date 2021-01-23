b02d345d66446b96eec2294d4e9b7ba256cc4135
Graham Martin <graham@pixely.co.uk>
2021-01-23 18:00:20 +0000

    feat: add lighthouse ci support

---
+++ lighthouserc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    ci: {
+        collect: {
+            url: [
+                "/",
+                "/2020-year-in-review/",
+                "/author/graham/",
+            ],
+            numberOfRuns: 2,
+            staticDistDir: "./public",
+        },
+        assert: {
+            preset: "lighthouse:recommended",   
+        },
+        upload: {
+            target: 'lhci',
+            serverBaseUrl: 'https://enigmatic-chamber-98135.herokuapp.com',
+            failOnUploadFailure: true,
+        },
+    },
+}
