faf607a9224a7e758ad62669ac6a727da0275880
Graham Martin <graham@pixely.co.uk>
2021-01-23 18:56:54 +0000

    chore: fix linting issues

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,17 +2,17 @@ module.exports = {
     ci: {
         collect: {
             url: [
-                "/",
-                "/2020-year-in-review/",
-                "/metrics-metrics-everywhere-where-to-start-with-web-performance-metrics/",
-                "/author/graham/",
-                "/tag/performance/",
+                '/',
+                '/2020-year-in-review/',
+                '/metrics-metrics-everywhere-where-to-start-with-web-performance-metrics/',
+                '/author/graham/',
+                '/tag/performance/',
             ],
             numberOfRuns: 3,
-            staticDistDir: "./dist",
+            staticDistDir: './dist',
         },
         assert: {
-            preset: "lighthouse:no-pwa",   
+            preset: 'lighthouse:no-pwa',   
         },
         upload: {
             target: 'lhci',
