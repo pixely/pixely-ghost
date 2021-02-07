86df825bc8c1d9b147e3a4547d884f08ab6d4d3f
Graham Martin <graham@pixely.co.uk>
2021-02-07 17:32:15 +0000

    chore: remove console logs

--- src/js/modules/ga.js
+++ src/js/modules/ga.js
@@ -2,17 +2,13 @@
 
 class GA {
     sendCustomEvent(eventName = null, propertiesObject = {}) {
-        console.log(eventName, propertiesObject)
         if (typeof eventName === 'string' && typeof gtag === 'function') {
-            console.log('gtag exists')
             gtag('event', eventName, propertiesObject)
         }
     }
 
     setUserProperties(propertiesObject = {}) {
-        console.log(propertiesObject)
         if (typeof gtag === 'function') {
-            console.log('gtag exists')
             gtag('set', 'user_properties', propertiesObject)
         }
     }
