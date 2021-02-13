4bdb106cd50c8078457723b5da05aaa9e73ecc9f
Graham Martin <graham@pixely.co.uk>
2021-02-13 20:24:31 +0000

    chore: minor changes to file names

--- src/_data/site.js
+++ src/_data/site.js
@@ -15,11 +15,9 @@ module.exports = async function () {
             console.error(err);
         });
     
-    // const icons = await generateImages(siteData.icon, [192, 512], ['png'])
-
     const iconSizes = [48, 192, 512, 1024];
     const iconFormats = ['png', 'svg'];
-    const iconRounded = await generateImages('src/images/favicons/favicon-round.svg', iconSizes, iconFormats);
+    const iconRounded = await generateImages('src/images/favicons/favicon.svg', iconSizes, iconFormats);
     const iconSquare = await generateImages('src/images/favicons/favicon-square.svg', iconSizes, iconFormats);
     
     const icons = {
