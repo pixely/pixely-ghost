8f023feb91e6c7f19ea8ad58e1e8112267277091
pixely <graham@pixely.co.uk>
2024-02-12 20:30:59 +0000

    chore: fix linting issues

--- lighthouse/lighthouse-server.js
+++ lighthouse/lighthouse-server.js
@@ -16,9 +16,9 @@ module.exports = {
                 skipAudits: ['redirects-http'],
             },
             assertions: {
-                'categories:performance': ['error', { 'minScore': 0.9}],
-                'categories:accessibility': ['error', { 'minScore': 0.9}],
-                'categories:seo': ['error', { 'minScore': 0.9}]
+                'categories:performance': ['error', { 'minScore': 0.9 }],
+                'categories:accessibility': ['error', { 'minScore': 0.9 }],
+                'categories:seo': ['error', { 'minScore': 0.9 }],
             },
         },
         // upload: {
